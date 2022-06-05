b80c5d94e8a16bbabf71704a1ea142fec771e414
jerfsonlink <jerfsonlink@gmail.com>
2022-06-05 15:09:47 -0300

    to fix code smells

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -6,37 +6,37 @@ const pageSize = 5;
 
 class UserService {
     async create({ name, email, password }) {
-        await userRepository.init()
-        const verifyEmail = await userRepository.findOneByEmail(email)
-        if (verifyEmail !== null) throw userErrorCode.emailAlreadyExists
+        await userRepository.init();
+        const verifyEmail = await userRepository.findOneByEmail(email);
+        if (verifyEmail !== null) throw userErrorCode.emailAlreadyExists;
 
         return userRepository.create({
             name,
             email,
-            password: await bcrypt.hash(password, 8)
+            password: await bcrypt.hash(password, 8),
         });
     }
     async update(id, { name, email, password }) {
-        await userRepository.init()
+        await userRepository.init();
 
-        const verifyId = await userRepository.findOneById(id)
-        if (verifyId === null) throw userErrorCode.userNotFound
+        const verifyId = await userRepository.findOneById(id);
+        if (verifyId === null) throw userErrorCode.userNotFound;
 
-        let data = {}
-        if (name) data.name = name
-        if (email) data.email = email
-        if (password) data.password = await bcrypt.hash(password, 8)
+        const data = {};
+        if (name) data.name = name;
+        if (email) data.email = email;
+        if (password) data.password = await bcrypt.hash(password, 8);
 
         return userRepository.update(id, data);
     }
     async delete(id) {
-        await userRepository.init()
-        return userRepository.delete(id)
+        await userRepository.init();
+        return userRepository.delete(id);
     }
     async findAll(filters, page) {
-        await userRepository.init()
-        const offset = (page - 1) * pageSize
-        return userRepository.findAll(filters, pageSize, offset)
+        await userRepository.init();
+        const offset = (page - 1) * pageSize;
+        return userRepository.findAll(filters, pageSize, offset);
     }
 }
 module.exports = new UserService();
